27b16892c8defc9ad36a8372800ba44dc50a2903
Jesse Tannahill <jesset@austinsigma.com>
2016-10-21 16:20:09 -0500

    Flesh out menu framework

--- main.c
+++ main.c
@@ -1,57 +1,78 @@
 #import <stdio.h>
 
-void menu() {
-    printf("1) Employees");
-    printf("2) Product Status");
-    printf("3) Quit");
-}
+void show_main();
 
-void menu_employees() {
-    // list employees
-    printf("1) Examine\n");
-    printf("2) Hire\n");
-    printf("3) First\n");
-    printf("4) Back\n");
+typedef struct {
+    char *name;
+    int age;
 
+    int speed;    /* how fast do they complete things */
+    int accuracy; /* how good is what they complete */
+} employee_t;
 
-}
+typedef void (*menu_cb_t)();
+typedef struct {
+    char *label;
+    menu_cb_t cb;
+} menu_entry_t;
 
-int menu_input(char *options[], int num_options) {
+void do_menu(menu_entry_t *entries, int num_entries) {
     int c;
     do {
         int i;
-        for (i = 0; i < num_options; i++) {
-            printf("%d) %s\n", i+1, options[i]);
+        for (i = 0; i < num_entries; i++) {
+            printf("%d) %s\n", i+1, entries[i].label);
         }
 
-        printf("\nEnter %d-%d: ", 1, num_options);
-        c = getchar() - '0';
-    } while (c < 0 || c > num_options);
+        printf("\nEnter %d-%d: ", 1, num_entries);
 
-    return c-1;
+        if (scanf("%d", &c) == -1) {
+            continue;
+        }
+        c--;
+    } while (c < 0 || c >= num_entries);
+
+    printf("\n"); //clean up
+
+    menu_cb_t cb = entries[c].cb;
+    if (cb) {
+        cb();
+    }
 }
 
+void print_title(char *title) {
+    printf("------%s------\n", title);
+}
+
+void show_product() {
+    print_title("PRODUCT STATUS");
+
+    menu_entry_t menu[] = {
+        {.label = "Product Status", .cb = show_product},
+        {.label = "Business"},
+        {.label = "Employees"},
+        {.label = "Back", .cb = show_main},
+    };
+
+    do_menu(menu, sizeof(menu)/sizeof(menu_entry_t));
+}
+
+void show_main() {
+    print_title("MAIN");
+    menu_entry_t menu[] = {
+        {.label = "Product Status", .cb = show_product},
+        {.label = "Employees"},
+        {.label = "Quit"},
+    };
+
+    do_menu(menu, sizeof(menu)/sizeof(menu_entry_t));
+}
 
 int main() {
     printf("Welcome to the Micromanager simulator");
     printf("You have been chosen as the manager for a failing business unit.\nPlease save the company!\n\n");
 
-    enum {
-        MAIN_OPTION_PRODUCT,
-        MAIN_OPTION_EMPLOYEE,
-        MAIN_OPTION_QUIT,
-        MAIN_OPTION_NUM,
-    };
-
-    char *main_options[] = {"Product Status", "Employees", "Quit"};
-
-    while (1) {
-        int option = menu_input(main_options, MAIN_OPTION_NUM);
-        if (option == MAIN_OPTION_QUIT) {
-            break;
-        }
-    }
-
+    show_main();
     printf("Bye!\n");
 
 	return 0;
